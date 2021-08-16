7055b693b44be135ee91e84626d6aead70052825
Josélio Júnior <76992016+joseliojunior@users.noreply.github.com>
2021-08-16 12:09:49 -0300

    refactoring

--- Lua_5.4/1187 - Área Superior.lua
+++ Lua_5.4/1187 - Área Superior.lua
@@ -3,18 +3,15 @@ local t = {}
 local x = 1
 local y = 12
 
+local sum = 0
 for i = 1, 5, 1 do
     t[i] = {}
     for j = 1, 12, 1 do t[i][j] = io.read() end
     for k = 1, x, 1 do t[i][k] = 0 end
     for k = 12, y, -1 do t[i][k] = 0 end
+    for j = 1, #t[i], 1 do sum = sum + t[i][j] end
     x = x + 1
     y = y - 1
-end
-
-local sum = 0
-for i = 1, 5, 1 do
-    for j = 1, #t[i], 1 do sum = sum + t[i][j] end
 end
 
 local total = operator == 'M' and sum / 30 or sum
