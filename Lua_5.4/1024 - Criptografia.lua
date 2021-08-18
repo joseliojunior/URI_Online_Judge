7393af2b50049f38a6a6ada61a8898c4445257e3
Josélio Júnior <76992016+joseliojunior@users.noreply.github.com>
2021-08-18 03:36:06 -0300

    Update 1024 - Criptografia.lua

--- Lua_5.4/1024 - Criptografia.lua
+++ Lua_5.4/1024 - Criptografia.lua
@@ -25,16 +25,14 @@ for _ = 1, io.read(), 1 do
     local s3 = ''
 
     local m = #s2 / 2
-    for i = 1, m , 1 do s3 = s3..s2[i] end
-    if #s2 % 2 == 0 then
-        for i = m + 1, #s2, 1 do
-            s3 = s3..string.char(utf8.codepoint(s2[i]) - 1)
-        end
-    else
-        for i = m + 0.5, #s2, 1 do
+    local function r(n)
+        for i = m + n, #s2, 1 do
             s3 = s3..string.char(utf8.codepoint(s2[i]) - 1)
         end
     end
+
+    for i = 1, m , 1 do s3 = s3..s2[i] end
+    if #s2 % 2 == 0 then r(1) else r(0.5) end
     table.insert(a, s3)
 end
 
