533202eeb4c1f2f94403c85697bc5df35b107433
Josélio Júnior <76992016+joseliojunior@users.noreply.github.com>
2021-08-25 13:53:18 -0300

    Update 1187 - Área Superior.lua

--- Lua_5.4/1187 - Área Superior.lua
+++ Lua_5.4/1187 - Área Superior.lua
@@ -9,7 +9,7 @@ local y = 12
 local sum = 0
 for i = 1, 5, 1 do
     t[i] = {}
-    for j = 1, 12, 1 do t[i][j] = io.read() end
+    for j = 1, 12, 1 do t[i][j] = io.read('n') end
     for k = 1, x, 1 do t[i][k] = 0 end
     for k = 12, y, -1 do t[i][k] = 0 end
     for j = 1, #t[i], 1 do sum = sum + t[i][j] end
