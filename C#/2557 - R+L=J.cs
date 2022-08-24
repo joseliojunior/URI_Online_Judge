f04c94f8aaf5e90075d5caa8fe394984fd34df5b
Josélio Júnior <76992016+lunatic-fox@users.noreply.github.com>
2022-08-24 12:18:51 -0300

    new

---
+++ C#/2557 - R+L=J.cs
@@ -0,0 +1,34 @@
+/// author Josélio de S. C. Júnior <joseliojrx25@gmail.com>
+/// copyright Josélio de S. C. Júnior 2022
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class URI {
+  static void Main(string[] args) { 
+    while (true) {
+      string r = Console.ReadLine() ?? "";
+      if (r == "") break;
+      var ra = Regex.Split(r, @"[+=]").ToList();
+      string k = Regex.Replace(string.Concat(ra), @"\d", "");
+      int rsp = 0;
+      var ira = ra.Select(e => int.Parse(Regex.Replace(e, @"[JLR]", "0"))).ToArray();
+      
+      switch (k) {
+        case "J":
+          rsp = ira[0] + ira[1];
+          break;
+        case "L":
+          rsp = (-ira[0]) + ira[2];
+          break;
+        case "R":
+          rsp = (-ira[1]) + ira[2];
+          break;
+      }
+      
+      Console.WriteLine(rsp);
+    }
+  }
+}
---
+++ C#/3159 - Tijolão.cs
@@ -0,0 +1,37 @@
+/// author Josélio de S. C. Júnior <joseliojrx25@gmail.com>
+/// copyright Josélio de S. C. Júnior 2022
+
+using System;
+using System.Linq;
+
+class URI {
+  static void Main(string[] args) { 
+    string[] ltt = { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+    int n = int.Parse(Console.ReadLine() ?? "1");
+    
+    for (int i = 0; i < n; i++) {
+      var r = (Console.ReadLine() ?? "")
+        .ToCharArray()
+        .Select(e => {
+          string lte = e.ToString();
+          string rsp = "";
+    
+          for (int j = 0; j < ltt.Length; j++) 
+            if (ltt[j].Contains(lte.ToLower())) 
+              rsp = new string(' ', ltt[j].IndexOf(lte.ToLower()) + 1).Replace(" ", (j + 2).ToString());
+    
+          if (lte == " ") rsp = "0";
+          if (lte == lte.ToUpper() && lte != " ") rsp = $"#{rsp}";
+    
+          return rsp;
+        })
+        .ToArray();
+  
+      for (int j = 0; j < r.Length; j++)
+        if (j + 1 < r.Length) 
+          if (r[j].Last() == r[j + 1].First()) r[j] = $"{r[j]}*";
+  
+      Console.WriteLine(string.Concat(r));
+    }
+  }
+}
