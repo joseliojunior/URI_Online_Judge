12a5afb82a4f10bd46b61e7fe2d94a8707080d0e
Lunatic Fox <joseliojrx25@gmail.com>
2023-08-10 23:52:51 -0300

    new

---
+++ readme.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+
+const obj = {}
+const langs = ['C', 'C#', 'Java', 'JavaScript', 'Kotlin', 'Lua', 'Pascal', 'Python'].map(e => fs.readdirSync(`./${e}`))
+  .flat()
+  .filter(e => e !== '.gitignore')
+
+langs.forEach(e => {
+  const title = e.replace(/(.+?)\.[^.]+$/, '$1')
+  let ex = e.replace(/.+?\.([^.]+)$/, '$1')
+
+  switch (ex) {
+    case 'cs':
+      ex = 'c-sharp'
+      break
+    case 'js':
+      ex = 'javascript'
+      break
+    case 'kt':
+      ex = 'kotlin'
+      break
+    case 'pas':
+      ex = 'pascal'
+      break
+    case 'py':
+      ex = 'python'
+      break
+  }
+
+  if (!obj[title]) obj[title] = []
+
+  const exw = ex === 'c-sharp' ? 'C#' : `${ex[0]?.toUpperCase()}${ex.slice(1)}`
+
+  obj[title].push(`[![](https://gh-tags.vercel.app/api?lang=${ex}&size=small)](https://github.com/lunatic-fox/uri-online-judge/blob/main/${exw}/${encodeURI(e)})`)
+
+})
+
+fs.writeFileSync('./README.md',
+  `# Beecrowd problem solutions
+  
+  <div align="center">
+
+  ## Please, if you are learning from the [beecrowd](https://www.beecrowd.com.br/) platform consult those solutions as your last resort, otherwise you probably will not learn the best as you should and/or get biased.
+  </div>\n\n${
+    Object.entries(obj)
+    .sort()
+    .map(([k, v]) => `- ${k} - ${v.join('&nbsp;')}`).join('\n')
+  }`
+)
