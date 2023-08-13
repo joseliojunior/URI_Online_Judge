a619ed202f051d83732038012c35d69b573967b6
Lunatic Fox <joseliojrx25@gmail.com>
2023-08-13 12:34:05 -0300

    new

--- .github/scripts/readme.js
+++ .github/scripts/readme.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 
 const obj = {}
-const langs = ['C', 'C#', 'Java', 'JavaScript', 'Kotlin', 'Lua', 'Pascal', 'Python'].map(e => fs.readdirSync(`./${e}`))
+const langs = ['C', 'C#', 'Java', 'JavaScript', 'Kotlin', 'Lua', 'Pascal', 'Python']
+  .map(e => fs.readdirSync(`./${e}`))
   .flat()
   .filter(e => e !== '.gitignore')
 
@@ -30,8 +31,8 @@ langs.forEach(e => {
   if (!obj[title]) obj[title] = []
 
   const exw = ex === 'c-sharp' ? 'C%23'
-: ex === 'javascript' ? 'JavaScript'
-: `${ex[0]?.toUpperCase()}${ex.slice(1)}`
+    : ex === 'javascript' ? 'JavaScript'
+    : `${ex[0]?.toUpperCase()}${ex.slice(1)}`
 
   obj[title].push(`[![](https://gh-tags.vercel.app/api?lang=${ex}&size=small)](https://github.com/lunatic-fox/uri-online-judge/blob/main/${exw}/${encodeURI(e)})`)
 
